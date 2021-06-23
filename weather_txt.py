52d67cf1c23bc3aa0774d53358cd744041e26cb6
sudharsanan-prog <85428326+sudharsanan-prog@users.noreply.github.com>
2021-06-23 13:30:16 +0530

    uploaded the project file

---
+++ weather_txt.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""weather.txt
+
+ginal file is located at
+    https://colab.research.google.com/drive/1DYM25VHbB4U6lK_lwVf_PYYm5pWJV4jM
+
+import requests
+#import os
+from datetime import datetime
+
+api_key = 'e5b3efa084ea770b8c42f17fb845c19c'
+location = input("Enter the city name: ")
+
+complete_api_link = "https://api.openweathermap.org/data/2.5/weather?q="+location+"&appid="+api_key
+api_link = requests.get(complete_api_link)
+api_data = api_link.json()
+
+#create variables to store and display data
+temp_city = ((api_data['main']['temp']) - 273.15)
+weather_desc = api_data['weather'][0]['description']
+hmdt = api_data['main']['humidity']
+wind_spd = api_data['wind']['speed']
+date_time = datetime.now().strftime("%d %b %Y | %I:%M:%S %p")
+
+print ("-------------------------------------------------------------")
+print ("Weather Stats for - {}  || {}".format(location.upper(), date_time))
+print ("-------------------------------------------------------------")
+
+print ("Current temperature is: {:.2f} deg C".format(temp_city))
+print ("Current weather desc  :",weather_desc)
+print ("Current Humidity      :",hmdt, '%')
+print ("Current wind speed    :",wind_spd ,'kmph')
+"""
